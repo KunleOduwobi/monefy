85f6aa92d4552454bf421757efeab9cafa990723
KunleOduwobi <kunleodu@gmail.com>
2026-02-03 15:54:32 +0100

    Set up project to launch app

--- src/test/java/androidTests/ViewSlides.java
+++ src/test/java/androidTests/ViewSlides.java
@@ -1,36 +1,29 @@
 package androidTests;
 
-import utils.AndroidSetup;
+import java.io.IOException;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.Onboarding;
+
+import base.AndroidSetup;
 
 public class ViewSlides extends AndroidSetup {
 
 	boolean fast;
 
 	@Parameters({ "BStackDeviceOS" })
-	@Test(description = "Verify that a user can skip welcome tour", groups = { "SkipOnboarding.skipWelcomeTour" })
-	public void skipWelcomeTour(String BStackDeviceOS) throws IOException {
+	@Test(description = "Verify that a user can view welcome tour", groups = { "ViewSlides.viewSlides" })
+	public void viewSlides(String BStackDeviceOS) throws IOException {
 
-		OnboardingPage onboardingPage = new OnboardingPage(driver);
-		NTPPage ntpPage = new NTPPage(driver);
+		Onboarding onboardingPage = new Onboarding(driver);
+//		NTPPage ntpPage = new NTPPage(driver);
 
-		Assert.assertEquals(onboardingPage.getFirstrunText(),
-				"The simplest way to be @ climate-active every day while @ browsing the web");
-		PromoDialog promoDialog = onboardingPage.skipTour();
+//		Assert.assertEquals(onboardingPage.getFirstrunText(),
+//				"The simplest way to be @ climate-active every day while @ browsing the web");
+//		PromoDialog promoDialog = onboardingPage.skipTour();
 
-		if (!TestName.equals("Promo Dialog Test")) {
-			if (onboardingPage.getFloatNumber(BStackDeviceOS) < 10.0) {
-				ntpPage.clickCloseTooltip();
-				ntpPage.customWaitForWebElement(ntpPage.getEcosiaLogo(), 60);
-
-			} else {
-//				if (onboardingPage.getFloatNumber(BStackDeviceOS) < 13.0) { // TEMP*****
-				promoDialog.customWaitForWebElement(promoDialog.getPromoDialog(), 5);
-				Assert.assertEquals(promoDialog.getPromoDialog().isDisplayed(), true);
-				promoDialog.clickPromoLaterBtn();
-				if (!TestName.equals("Ntp Test"))
-					ntpPage.clickCloseTooltip();
-//				}
-			}
+		
 
 //			if (onboardingPage.getFloatNumber(BStackDeviceOS) > 12.0) {
 //				try {
@@ -47,20 +40,20 @@ public class ViewSlides extends AndroidSetup {
 //					ntpPage.clickCloseTooltip();
 //			}
 
-			try {
-				long startTime = System.currentTimeMillis();
-				ntpPage.customWaitForWebElement(ntpPage.getNewsImageItem(), 5);
-				long estimatedTime = System.currentTimeMillis() - startTime;
-				if (estimatedTime > 5000) {
-					fast = false;
-				} else {
-					fast = true;
-				}
-			} catch (Exception e) {
-				fast = false;
-
-			}
-		}
+//			try {
+//				long startTime = System.currentTimeMillis();
+//				ntpPage.customWaitForWebElement(ntpPage.getNewsImageItem(), 5);
+//				long estimatedTime = System.currentTimeMillis() - startTime;
+//				if (estimatedTime > 5000) {
+//					fast = false;
+//				} else {
+//					fast = true;
+//				}
+//			} catch (Exception e) {
+//				fast = false;
+//
+//			}
+//		}
 
 	}
 
